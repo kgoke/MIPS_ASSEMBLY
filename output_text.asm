d18377320527595b6f7758f1b12aaea178d71986
Keegan Goecke <89423561+kgoke@users.noreply.github.com>
2022-09-15 23:51:43 -0400

    how to print a string

--- output_text.asm
+++ output_text.asm
@@ -1,11 +1,11 @@
-      .data
+      .data #tells assembler to expext data
 one:  .asciiz "This is a string!"
-      .text
+      .text # tells assembler ot expect code
 
 main:
-      li $v0, 4
-      la $a0, one
-      syscall
+      li $v0, 4   # loads 4 into v0 to get print a string
+      la $a0, one # loads one into a0
+      syscall     # calls to OS
       
-      li $v0, 10
-      syscall
+      li $v0, 10  # loads 10 into v0 to quit
+      syscall     # calls OS
